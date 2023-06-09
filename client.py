26e23759271ab1980570ff81e1aa177e94885ec7
Reuben <reuben_emmens@outlook.com>
2023-06-09 16:33:27 +0100

    Complete base functionality. Update secret key to match.

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ from stem.control import Controller
 def renew_tor_circuit():
     '''Renew the Tor circuit and generate a new IP address.'''
     # Access password as secret environment variable
-    password = os.environ.get('tor-password')
+    password = os.environ.get('TOR_PASSWORD')
 
     with Controller.from_port(port=9051) as controller: # type: ignore
         controller.authenticate(password=password)
