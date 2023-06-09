5cefc8691cca9c3f227c4c4c532b171eda77cfb4
Reuben <reuben_emmens@outlook.com>
2023-06-09 12:43:20 +0100

    Create script and Dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.10-slim
+
+# Install Tor
+RUN apt-get update && apt-get install -y tor
+# Remove the default torrc file
+RUN rm /etc/tor/torrc
+# Copy your modified torrc file into the container
+COPY torrc /etc/tor/torrc
+# Expose the Tor control port
+EXPOSE 9051
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN python -m pip install --upgrade pip && \
+    python -m pip install --no-cache-dir -r requirements.txt
+
+COPY tor_socks.py .
+
+# Creates a non-root user with an explicit UID and adds permission to access the /app folder
+# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+USER appuser
+
+CMD ["python", "tor_socks.py"]
---
+++ tor_socks.py
@@ -0,0 +1,44 @@
+'''This script demonstrates how to use Tor with Python to make requests over the Tor network.'''
+import cProfile
+import os
+import pstats
+
+import requests
+from stem import Signal
+from stem.control import Controller
+
+def renew_tor_circuit():
+    '''Renew the Tor circuit and generate a new IP address.'''
+    # Access password as secret environment variable
+    password = os.environ.get('torPassword')
+
+    with Controller.from_port(port=9051) as controller: # type: ignore
+        controller.authenticate(password=password)
+        controller.signal(Signal.NEWNYM) # type: ignore # pylint: disable=no-member
+
+def make_request(url):
+    '''Make a request over the Tor network.'''
+    renew_tor_circuit()
+
+    timeout = 120
+
+    proxy = {
+        'http': 'socks5://127.0.0.1:9050',
+        'https': 'socks5://127.0.0.1:9050'
+    }
+
+    response = requests.get(url, proxies=proxy, timeout=timeout)
+    print(response.text)
+
+def main():
+    '''Entry point to the program'''
+    url = 'https://checkip.amazonaws.com'
+    cProfile.run(f'make_request("{url}")', 'profile_stats')
+
+    stats = pstats.Stats('profile_stats')
+    stats.strip_dirs()
+    stats.sort_stats('cumulative')  # Sort by cumulative time
+    stats.print_stats()
+
+if __name__ == '__main__':
+    main()
